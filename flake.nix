76cfb565122d450402fef1008d18f02938fe1ab5
Nathanael Robbins <nejrobbins@gmail.com>
2024-01-01 16:10:26 -0500

    Special system attribute for devshell
    Without it, the devshell declared in flake.nix wasn't being used, and
    instead `nix develop` was using brightness-notifyd.nix and entering a
    shell meant for running the individual Nix build phases

--- flake.nix
+++ flake.nix
@@ -13,12 +13,14 @@
   in {
     packages.x86_64-linux.brightness-libnotify = pkgs.callPackage ./brightness-libnotify.nix { stdenv = pkgs.clangStdenv; };
     packages.x86_64-linux.default = self.packages.x86_64-linux.brightness-libnotify;
-    devShells.default = pkgs.mkShell {
+    devShells."x86_64-linux".default = pkgs.mkShell {
       packages = with pkgs; [
         clang-tools
+        clang
         glib.dev
         gdk-pixbuf.dev
         pkg-config
+        libnotify.dev
       ];
     };
   };
